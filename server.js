cfaf78d7817a74a8015453598a04df5cc7950a92
sanaAB <sanaabohelal@gmail.com>
2021-06-19 10:42:58 +0200

    project setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+console.log("hello from cookbook server");

8c944ba03a45703ff0297b7257fc405b3c294a4c
sanaAB <sanaabohelal@gmail.com>
2021-06-30 14:33:52 +0200

    fixed the empty array problem

--- src/database.js
+++ src/database.js
@@ -1,15 +1,13 @@
-const express = require("express");
 const mongoose = require("mongoose");
 
 const url = process.env.DATABASE_URL;
-console.log(process.env.DATABASE_URL);
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+	useFindAndModify: false
 });
-
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 module.exports = db;
